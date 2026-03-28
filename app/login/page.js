9c9833bd3b9860a4d6c5fad88d78316805bff330
oesalv <oesalv@gmail.com>
2026-03-28 14:41:31 +0100

    Delete app/login directory

--- app/login/page.js
+++
@@ -1,8 +0,0 @@
-export default function LoginPage() {
-  return (
-    <main style={{ padding: 40, fontFamily: "Arial" }}>
-      <h1>Login test</h1>
-      <p>Denne siden virker.</p>
-    </main>
-  );
-}
