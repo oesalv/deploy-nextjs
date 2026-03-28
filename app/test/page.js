5ee17e169be2511ea78953ad003c1cc8578357bd
oesalv <oesalv@gmail.com>
2026-03-28 14:53:13 +0100

    ny side

---
+++ app/test/page.js
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Testside</h1>
+      <p>Denne er bare for å sjekke routing.</p>
+    </main>
+  );
+}
